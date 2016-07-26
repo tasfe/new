aaac232b6f6267bb5e75eb50d30b2ceefe7d8753
123 <123@jishu-10>
2016-07-26 11:27:26 +0800

    1

--- global-forehead_fh/src/apps/main/bettingCenter/views/bettingCenter-quickBet.js
+++ global-forehead_fh/src/apps/main/bettingCenter/views/bettingCenter-quickBet.js
@@ -11,11 +11,23 @@ var QuickBetView = Base.ItemView.extend({
   onRender: function() {
 
     var self = this;
-    var str = 	'<div class="js-bc-quick-btn nc-board-item clearfix router" data-affiche="111">11111111</div></br>'+
-      '<div class="js-bc-quick-btn nc-board-item clearfix router" data-affiche="222">22222222</div></br>'+
-      '<div class="js-bc-quick-btn nc-board-item clearfix router" data-affiche="3333">222222</div>'+
+    var str = 	'<div class="js-bc-quick-btn control-inlin-num" data-affiche="100">100元</div>'+
+                '<div class="js-bc-quick-btn control-inlin-num" data-affiche="200">200元</div>'+
+                '<div class="js-bc-quick-btn control-inlin-num" data-affiche="300">300元</div>'+
+                '<div class="js-bc-quick-btn control-inlin-num" data-affiche="400">400元</div>'+
+                '<div class="js-bc-quick-btn control-inlin-num" data-affiche="500">500元</div>'+
+                '<div class="js-bc-quick-btn control-inlin-num" data-affiche="600">600元</div></br>'+
 
-      '<div class="js-bc-quick-btn text-left" > 单击 </div>'+
+                '<div class="js-bc-quick-btn control-inlin-num" data-affiche="800">800元</div>'+
+                '<div class="js-bc-quick-btn control-inlin-num" data-affiche="1000">1000元</div>'+
+                '<div class="js-bc-quick-btn control-inlin-num" data-affiche="1500">1500元</div>'+
+                '<div class="js-bc-quick-btn control-inlin-num" data-affiche="2000">2000</div>'+
+                '<div class="js-bc-quick-btn control-inlin-num" data-affiche="3000">3000</div>'+
+                '<div class="js-bc-quick-btn control-inlin-num" data-affiche="5000">5000</div></br>'+
+
+                '<div class="js-bc-quick-btn control-inlin-num" data-affiche="">自定义</div></br>'+
+
+      '<div class="js-bc-quick-btn text-left" > 确认投注 </div>'+
 
       '</div>';
 
