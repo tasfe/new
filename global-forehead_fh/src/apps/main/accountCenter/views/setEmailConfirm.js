fdaf9afd98f226ddfae0930ecec6f4793f4f5ee5
xiaodian2014 <2366184417@qq.com>
2016-08-11 21:59:16 +0800

    dsfs

--- global-forehead_fh/src/apps/main/accountCenter/views/setEmailConfirm.js
+++ global-forehead_fh/src/apps/main/accountCenter/views/setEmailConfirm.js
@@ -9,10 +9,8 @@ var confirmEmailView = Base.ItemView.extend({
     className: 'as-securityQuestion-view',
 
     events: {
-
         'click .js-fd-finished': 'nextHandler', //输入密保问题(与修改页面共用)
         'click .js-acse-back': 'acseBackHandler'
-
     },
 
     initialize: function () {
@@ -21,38 +19,36 @@ var confirmEmailView = Base.ItemView.extend({
 
     onRender: function () {
 
-        this.$('.js-fd-finished');
+        var self = this;
+
+        this.$('.js-fd-finished').attr("disabled","disabled");
 
         this.$('.js-email-reality').html(this.options.email);
-
         this.$('.js-emailTime').html(50);
-        alert(this.$('.js-emailTime').html());
+
         var emailTime = setInterval(function(){
             var num = $('.js-emailTime').html() - 1;
             if ($('.js-emailTime').html() == 0) {
-                clearInterval(emailTime)
+                clearInterval(emailTime);
+                self.$('.js-fd-finished').removeAttr("disabled");
             }else{
                 $('.js-emailTime').html(num);
             }
         }, 1000)
-
-
-
+        
     },
 
     nextHandler: function () {
-
         $('.js-acse-container').html(this.finish());
-
     },
 
     acseBackHandler:function () {
-
+        
         var ContentEmailView12 = require('accountCenter/views/contentEmail');
         var conetent = new ContentEmailView12();
         $('.js-acse-container').html(conetent.render().el);
         this.destroy();
-
+        
     }
 
 
