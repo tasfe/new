237cfdeac402816beb8adda7810609b025333c63
polo <polo@123.com>
2016-09-30 14:03:38 +0800

    footer complete

--- global-forehead_fh/src/apps/components/footer/index.js
+++ global-forehead_fh/src/apps/components/footer/index.js
@@ -9,38 +9,13 @@ var FooterView = Base.ItemView.extend({
   render: function() {
     this.$el.html(_(this.template).template()(this.options));
 
-    this.$('.footer-title-android').popover({
-      trigger: 'click',
-      html: true,
-      container: '.footer-android-main',
-      content: '<div class="js-gl-popover footer-popover"><img src="'+ require('./android-qrcode.png')+'"</div>',
-      placement: 'top'
-    });
-
-    this.$('.footer-title-apple').popover({
-      trigger: 'click',
-      html: true,
-      container: '.footer-apple-main',
-      content: '<div class="js-gl-popover footer-popover"><img src="'+ require('./ios-qrcode.png')+'"</div>',
-      placement: 'top'
-    });
-
-    this.$('.footer-title-h5').popover({
-      trigger: 'click',
-      html: true,
-      container: '.footer-h5-main',
-      content: '<div class="js-gl-popover footer-popover"><img src="'+ require('./h5-qrcode.png')+'"</div>',
-      placement: 'top'
-    });
-
-
-    this.$('.footer-title-wechat').popover({
-      trigger: 'click',
-      html: true,
-      container: '.footer-wechat-main',
-      content: '<div class="js-gl-popover footer-popover"><img src="'+ require('./wechat-qrcode.png')+'"></div>',
-      placement: 'top'
-    });
+    // this.$('.footer-title-wechat').popover({
+    //   trigger: 'click',
+    //   html: true,
+    //   container: '.footer-wechat-main',
+    //   content: '<div class="js-gl-popover footer-popover"><img src="'+ require('./wechat-qrcode.png')+'"></div>',
+    //   placement: 'top'
+    // });
 
   }
 });
