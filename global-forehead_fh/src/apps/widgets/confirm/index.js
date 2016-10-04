5415b9702fd8ad4f83c0541c42a75a4f887f6b00
mashiro <641047675@qq.com>
2016-10-04 17:15:59 +0800

    notification and dialog

--- global-forehead_fh/src/apps/widgets/confirm/index.js
+++ global-forehead_fh/src/apps/widgets/confirm/index.js
@@ -6,7 +6,7 @@ $.widget('gl.confirm', {
 
   options: {
     id: '',
-    size:  '',
+    size: '',
     namespace: 'confirm',
     agreeCallback: _.noop,
     rejectCallback: _.noop,
@@ -17,44 +17,45 @@ $.widget('gl.confirm', {
     btnRightText: '取消'
   },
 
-  _create: function() {
+  _create: function () {
 
     var self = this;
 
     this.uuid = this.options.id || 'confirm-' + _.now();
     var body = [];
     var footer = '<div class="text-center">' +
-    '<button type="button" class="js-confirm-agree btn btn-pink btn-linear" data-loading-text="保存中">'+this.options.btnLeftText+'</button>' +
-    '<button type="button" class="js-confirm-reject btn btn-linear" data-dismiss="modal">'+this.options.btnRightText+'</button></div>';
+      '<button type="button" class="js-confirm-agree btn btn-pink btn-linear" data-loading-text="保存中">' + this.options.btnLeftText + '</button>' +
+      '<button type="button" class="js-confirm-reject btn btn-linear" data-dismiss="modal">' + this.options.btnRightText + '</button></div>';
 
-      var data = {
+    var data = {
+      id: this.uuid,
+      title: this.options.title,
+      body: this.options.content,
+      size: this.options.size,
+      footer: this.options.noFooter ? '' : footer
+    };
+
+    if (this.options.type === 'exit') {
+      body.push('<div class="m-TB-md text-center"><span class="sfa sfa-dialog-info"></span></div>');
+      body.push('<div class="text-center font-md">' + this.options.content + '</div>');
+
+      footer = '<div class="text-center">' +
+        '<button type="button" class="js-confirm-agree btn btn-pink btn-linear" data-loading-text="处理中">' + this.options.btnLeftText + '</button>' +
+        '<button type="button" class="js-confirm-reject btn btn-linear" data-dismiss="modal">' + this.options.btnRightText + '</button></div>';
+      // body.push(footer);
+
+      data = {
+        size: 'modal-sm',
+        modalClass: 'modal-notification',
         id: this.uuid,
-        title: this.options.title,
-        body: this.options.content,
-        size: this.options.size,
-        footer: this.options.noFooter ? '' : footer
+        body: body.join(''),
+        footer: footer
       };
-
-     if (this.options.type === 'exit') {
-       body.push('<div class="m-TB-md text-center"><span class="sfa sfa-dialog-info"></span></div>');
-       body.push('<div class="text-center font-md">' + this.options.content + '</div>');
-
-       footer = '<div class="m-TB-md text-center"><div class="text-center">' +
-        '<button type="button" class="js-confirm-agree btn btn-pink btn-linear" data-loading-text="处理中">'+this.options.btnLeftText+'</button>' +
-         '<button type="button" class="js-confirm-reject btn btn-linear" data-dismiss="modal">'+this.options.btnRightText+'</button></div>';
-       body.push(footer);
-
-       data = {
-         size: 'modal-sm',
-         modalClass: 'modal-notification',
-         id: this.uuid,
-         body: body.join('')
-       };
     }
 
     this.$dialog = Global.ui.dialog.show(data);
 
-    this.$dialog.on('hidden.modal', function(e) {
+    this.$dialog.on('hidden.modal', function (e) {
       $(this).remove();
       self.destroy();
     });
@@ -62,7 +63,7 @@ $.widget('gl.confirm', {
     this._bindEvents();
   },
 
-  _bindEvents: function() {
+  _bindEvents: function () {
 
     this._on({
       'click .js-confirm-agree': 'agreeConfirmHandler',
@@ -70,17 +71,17 @@ $.widget('gl.confirm', {
     });
   },
 
-  show: function() {
+  show: function () {
     Global.ui.dialog.show({
       id: this.uuid
     });
   },
 
-  hide: function() {
+  hide: function () {
     this.$dialog.modal('hide');
   },
 
-  agreeConfirmHandler: function(e) {
+  agreeConfirmHandler: function (e) {
 
     this.options.agreeCallback(e);
 
@@ -89,7 +90,7 @@ $.widget('gl.confirm', {
     return false;
 
   },
-  rejectConfirmHandler: function(e) {
+  rejectConfirmHandler: function (e) {
 
     this.options.rejectCallback();
 
