56b28c590f461ec0c5becbfce400614f25ac7bc5
mashiro <641047675@qq.com>
2016-10-06 10:10:40 +0800

    reformat carousel

--- global-forehead_fh/src/apps/main/dashboard/router.js
+++ global-forehead_fh/src/apps/main/dashboard/router.js
@@ -6,8 +6,7 @@ exports.install = function() {
 
   window.Global.appRouter.processAppRoutes(new DashboardController(), {
     '': 'dashboard', // 概览
-    ':anything': 'dashboard', // 概览
-    //'nb/detail/:noticeId': 'noticeDetail'//公告详情
+    ':anything': 'dashboard' // 概览
   });
 
 };
