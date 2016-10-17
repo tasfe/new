0046a031e38d030708f0b160ccb013d14d865036
bill <bill@123.com>
2016-10-17 20:41:30 +0800

    helpCenter

--- global-forehead_fh/src/apps/components/sidebar-multi-help/index.js
+++ global-forehead_fh/src/apps/components/sidebar-multi-help/index.js
@@ -25,7 +25,7 @@ var MultiSidebarView = Base.ItemView.extend({
 
     this.$multiMain.slimScroll({
       height: this.options.height,
-      alwaysVisible: true
+      alwaysVisible: false
     });
     this.$('.js-com-multi-item').eq(0).trigger('click');
   },
--- global-forehead_fh/src/apps/main/helpCenter/index/config.js
+++ global-forehead_fh/src/apps/main/helpCenter/index/config.js
@@ -10,50 +10,29 @@ var helpConfig = {
       icon: require('./help-new.png'),
       sub: [
         {
-          args: 'page=newbie',
+          args: 'page=gr-aboutUs',
           name: '关于繁华',
-          html: require('helpCenter/faq/newbie.html')
+          html: require('helpCenter/greenhand/aboutUs.html')
         },
         {
-          args: 'page=charge-withdrawal',
+          args: 'page=gr-register',
           name: '注册和购彩',
-          html: require('helpCenter/faq/charge-withdrawal.html')
+          html: require('helpCenter/greenhand/register.html')
         },
         {
-          args: 'page=game',
+          args: 'page=gr-function',
           name: '平台功能',
-          html: require('helpCenter/faq/game.html')
+          html: require('helpCenter/greenhand/function.html')
         },
         {
-          args: 'page=safe',
+          args: 'page=gr-feature',
           name: '繁华特色',
-          html: require('helpCenter/faq/safe.html')
+          html: require('helpCenter/greenhand/feature.html')
         },
         {
-          args: 'page=safe',
+          args: 'page=gr-agency',
           name: '代理繁华',
-          html: require('helpCenter/faq/safe.html')
-        },
-        //todo 分割
-        {
-          args: 'page=newbie',
-          name: '新手类',
-          html: require('helpCenter/faq/newbie.html')
-        },
-        {
-          args: 'page=charge-withdrawal',
-          name: '充提类',
-          html: require('helpCenter/faq/charge-withdrawal.html')
-        },
-        {
-          args: 'page=game',
-          name: '游戏类',
-          html: require('helpCenter/faq/game.html')
-        },
-        {
-          args: 'page=safe',
-          name: '安全类',
-          html: require('helpCenter/faq/safe.html')
+          html: require('helpCenter/greenhand/agency.html')
         }
       ]
     },
@@ -62,55 +41,29 @@ var helpConfig = {
       icon: require('./help-introduce.png'),
       sub: [
         {
-          args: 'page=ssc',
+          args: 'page=tickets-ssc',
           name: '时时彩玩法介绍',
           html: require('helpCenter/tickets/ssc.html')
         },
         {
-          args: 'page=num',
+          args: 'page=tickets-num',
           name: '11 选 5 玩法介绍',
           html: require('helpCenter/tickets/num.html')
         },
         {
-          args: 'page=low',
+          args: 'page=tickets-low',
           name: '低频彩玩法介绍',
           html: require('helpCenter/tickets/low.html')
         },
         {
-          args: 'page=ssc',
+          args: 'page=tickets-sp',
           name: '繁华专享彩种玩法介绍',
-          html: require('helpCenter/tickets/ssc.html')
+          html: require('helpCenter/tickets/sp.html')
         },
         {
-          args: 'page=happy',
-          name: '快乐彩玩法介绍',
+          args: 'page=tickets-happy',
+          name: '北京PK10玩法介绍',
           html: require('helpCenter/tickets/happy.html')
-        },
-        //todo 分割
-        //{
-        //  args: 'page=gh-recharge',
-        //  name: '工行在线充值流程',
-        //  html: require('helpCenter/recharge/gh-recharge.html')
-        //},
-        //{
-        //  args: 'page=gh-receipt',
-        //  name: '工行回执单查询',
-        //  html: require('helpCenter/recharge/gh-receipt.html')
-        //},
-        {
-          args: 'page=quick-top-up',
-          name: '快捷充值流程',
-          html: require('helpCenter/recharge/quick-top-up.html')
-        },
-        {
-          args: 'page=quick-top-up-order',
-          name: '快捷充值支付订单号',
-          html: require('helpCenter/recharge/quick-top-up-order.html')
-        },
-        {
-          args: 'page=charge-notice',
-          name: '充值须知',
-          html: require('helpCenter/recharge/charge-notice.html')
         }
       ]
     },
@@ -119,35 +72,24 @@ var helpConfig = {
       icon: require('./help-description.png'),
       sub: [
         {
-          args: 'page=withdrawal-flow',
+          args: 'page=gi-openException',
           name: '开奖异常',
-          html: require('helpCenter/withdrawal/flow.html')
+          html: require('helpCenter/gameInstruction/openException.html')
         },
         {
-          args: 'page=withdrawal-question',
+          args: 'page=gi-chaseBet',
           name: '追号投注',
-          html: require('helpCenter/withdrawal/question.html')
+          html: require('helpCenter/gameInstruction/chaseBet.html')
         },
         {
-          args: 'page=withdrawal-flow',
+          args: 'page=gi-cancelBet',
           name: '撤单功能',
-          html: require('helpCenter/withdrawal/flow.html')
+          html: require('helpCenter/gameInstruction/cancelBet.html')
         },
         {
-          args: 'page=withdrawal-question',
+          args: 'page=gi-malicious',
           name: '恶投和冷热号',
-          html: require('helpCenter/withdrawal/question.html')
-        },
-        //todo 分割
-        {
-          args: 'page=withdrawal-flow',
-          name: '平台提款流程',
-          html: require('helpCenter/withdrawal/flow.html')
-        },
-        {
-          args: 'page=withdrawal-question',
-          name: '平台提款到账时间问题',
-          html: require('helpCenter/withdrawal/question.html')
+          html: require('helpCenter/gameInstruction/malicious.html')
         }
       ]
     },
@@ -156,76 +98,34 @@ var helpConfig = {
       icon: require('./help-yan.png'),
       sub: [
         {
-          args: 'page=forget-pwd',
+          args: 'page=rw-ways',
           name: '充提方式',
-          html: require('helpCenter/account/forget-pwd.html')
+          html: require('helpCenter/rechargeWithdraw/ways.html')
         },
         {
-          args: 'page=forget-fund-pwd',
+          args: 'page=rw-flows',
           name: '充提流程',
-          html: require('helpCenter/account/forget-fund-pwd.html')
+          html: require('helpCenter/rechargeWithdraw/flows.html')
         },
         {
-          args: 'page=security-question',
+          args: 'page=rw-instructions',
           name: '充提说明',
-          html: require('helpCenter/account/security-question.html')
+          html: require('helpCenter/rechargeWithdraw/instructions.html')
         },
         {
-          args: 'page=forget-question',
+          args: 'page=rw-exception',
           name: '充提异常',
-          html: require('helpCenter/account/forget-question.html')
+          html: require('helpCenter/rechargeWithdraw/exception.html')
         },
         {
-          args: 'page=contact-superior',
+          args: 'page=rw-unionReceipt',
           name: '银联回单查询',
-          html: require('helpCenter/account/contact-superior.html')
+          html: require('helpCenter/rechargeWithdraw/unionReceipt.html')
         },
         {
-          args: 'page=contact-inferior',
+          args: 'page=rw-wechatReceipt',
           name: '微信回单查询',
-          html: require('helpCenter/account/contact-inferior.html')
-        },
-        //todo
-
-        {
-          args: 'page=forget-pwd',
-          name: '忘记登录密码怎么办？',
-          html: require('helpCenter/account/forget-pwd.html')
-        },
-        {
-          args: 'page=forget-fund-pwd',
-          name: '忘记资金密码怎么办？',
-          html: require('helpCenter/account/forget-fund-pwd.html')
-        },
-        {
-          args: 'page=security-question',
-          name: '保密问题基本介绍',
-          html: require('helpCenter/account/security-question.html')
-        },
-        {
-          args: 'page=forget-question',
-          name: '忘记密保答案怎么办？',
-          html: require('helpCenter/account/forget-question.html')
-        },
-        {
-          args: 'page=contact-superior',
-          name: '如何联系我的上级？',
-          html: require('helpCenter/account/contact-superior.html')
-        },
-        {
-          args: 'page=contact-inferior',
-          name: '如何联系我的下级？',
-          html: require('helpCenter/account/contact-inferior.html')
-        },
-        {
-          args: 'page=search-ip',
-          name: '如何查询当前IP地址？',
-          html: require('helpCenter/account/search-ip.html')
-        },
-        {
-          args: 'page=cancel-notice',
-          name: '如何取消系统通知？',
-          html: require('helpCenter/account/cancel-notice.html')
+          html: require('helpCenter/rechargeWithdraw/wechatReceipt.html')
         }
       ]
     },
@@ -234,21 +134,20 @@ var helpConfig = {
       icon: require('./help-security.png'),
       sub: [
         {
-          args: 'page=ssc',
+          args: 'page=se-forget-pwd',
           name: '忘记密码',
-          html: require('helpCenter/tickets/ssc.html')
+          html: require('helpCenter/security/forget-pwd.html')
         },
         {
-          args: 'page=num',
+          args: 'page=se-accountSecurity',
           name: '账户安全',
-          html: require('helpCenter/tickets/num.html')
+          html: require('helpCenter/security/accountSecurity.html')
         },
         {
-          args: 'page=low',
+          args: 'page=se-bankCardMana',
           name: '管理银行卡',
-          html: require('helpCenter/tickets/low.html')
-        },
-
+          html: require('helpCenter/security/bankCardMana.html')
+        }
       ]
     },
     {
@@ -256,16 +155,15 @@ var helpConfig = {
       icon: require('./help-client.png'),
       sub: [
         {
-          args: 'page=client',
+          args: 'page=client-pc',
           name: ' PC客户端',
-          html: require('helpCenter/tickets/ssc.html')
+          html: require('helpCenter/client/client-pc.html')
         },
         {
-          args: 'page=num',
+          args: 'page=client-mobile',
           name: '手机移动端',
-          html: require('helpCenter/tickets/num.html')
-        },
-
+          html: require('helpCenter/client/client-mobile.html')
+        }
       ]
     }
   ],
