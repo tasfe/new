92eb7dc943dbc3f2624878bee6831220472d3f74
athena <athena@123.com>
2016-10-20 17:35:28 +0800

    首充送，红包大放送，绑卡送活动v1

---
+++ global-forehead_fh/src/apps/external/activity/redpacket/index.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// var config = require('com/newrecharge/config');
+
+var RedPacket = Base.ItemView.extend({
+
+  template: require('./index.html'),
+  events: {
+  },
+  initialize: function() {
+    var self = this;
+    require.ensure(['./index.scss'], function(require) {
+      require('./index.scss');
+      window.setTimeout(function() {
+        self._onRender();
+      }, 10);
+    });
+	  $(window).on('resize.resizeview', this.onResize.bind(this));
+  },
+  _onRender: function() {
+    this.setAllSize();
+  },
+	onResize: function () {
+		this.setAllSize();
+	},
+  setAllSize: function(){
+    var wh = $(window).height();
+	  var bgw = 1920 * (wh / 1024);
+
+    this.$('.activity_mask').css({
+    	'height' : wh,
+	    'background-size' : bgw + 'px ' + wh + 'px'
+    });
+  },
+	//handler event
+});
+
+module.exports = RedPacket;
