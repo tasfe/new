d2724ae677c0a8ddb85b0592e465c78e93d90991
mashiro <641047675@qq.com>
2016-10-25 19:28:06 +0800

    rsync

--- global-forehead_fh/local_modules/rsync/index.js
+++ global-forehead_fh/local_modules/rsync/index.js
@@ -3,13 +3,13 @@ var path = require('path');
 var rsync = require('gulp-rsync');
 
 gulp.task('rsync', function() {
-  console.log(gulp.src(path.resolve('./local_modules') + '/mockup/dev-proxy.js'));
+  // console.log(gulp.src(path.resolve('./local_modules') + '/mockup/dev-proxy.js'));
   return gulp.src(path.resolve('./local_modules') + '/mockup/dev-proxy.js')
     .pipe(rsync({
       progress: true,
-      root: path.resolve('./local_modules'),
+      root: path.resolve('./local_modules/mockup'),
       hostname: '52.196.220.175',
       username: 'root',
-      destination: '/data/test'
+      destination: '/data/test/'
     }));
 });
